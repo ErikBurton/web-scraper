077735e148e23def618ab31f246c603c1b1c7a50
Erik Burton <erikburton1@gmail.com>
2021-11-07 17:25:20 -0500

    Initialized express and will use PORT 8000

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const PORT = 8000
 const axios = require('axios')
 const cherrio = require('cherrio')
 const express = require('express')
 
 const app = express()
+
+app.listenerCount(PORT, () => console.log(`server running on PORT ${PORT}`))
