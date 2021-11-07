c6262f863c24701a8ee6823a849b726865f14a95
Erik Burton <erikburton1@gmail.com>
2021-11-07 17:29:03 -0500

    modified index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const PORT = 8000
 const axios = require('axios')
-const cherrio = require('cherrio')
+const cheerio = require('cheerio')
 const express = require('express')
 
 const app = express()
 
-app.listenerCount(PORT, () => console.log(`server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+
