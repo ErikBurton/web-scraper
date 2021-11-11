b60d404aa6e512fe1bd0fb11b96a671e00f4137a
Erik Burton <erikburton1@gmail.com>
2021-11-10 19:23:00 -0500

    Modified app.js file to get article title and url

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ const feedDisplay = document.querySelector('#feed')
 
 fetch('http://localhost:8000/results')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.forEach(article => {
+            const articleItem = '<div><h3>' + article.title + '</h3><p>' + article.url + '</p></div>'
+            feedDisplay.insertAdjacentHTML("beforeend", articleItem)
+        })
+    }) 
